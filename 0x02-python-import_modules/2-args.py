78634e800c0720d3da5390b2867c33b87c102978
mateo-a <alexander.urrego@gmail.com>
2019-12-12 18:25:32 -0500

    [Add] Add files for tasks 1 and 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+        len_arg = len(argv)
+        if len_arg == 1:
+                print("0 arguments.")
+        elif len_arg == 2:
+                print("1 argument:")
+                print("1: {}".format(argv[1]))
+        elif len_arg > 2:
+                print("{} arguments:".format(len_arg -1))
+                for i in range(1, len_arg):
+                        print("{}: {}".format(i, argv[i]))
