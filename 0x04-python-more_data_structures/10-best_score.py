a821c96000514c29e19617a37fc032b4cecc5c79
mateo-a <alexander.urrego@gmail.com>
2019-12-19 21:05:45 -0500

    [Fix] Corrected PEP8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
     sort_k = sorted(a_dictionary.keys(), key=lambda x: a_dictionary[x],
-                         reverse=True)
+                    reverse=True)
     return (sort_k[0])
