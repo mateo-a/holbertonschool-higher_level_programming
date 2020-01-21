8f3075d71acf2f52319274b2dcd926f480dc1c46
mateo-a <alexander.urrego@gmail.com>
2020-01-21 08:41:58 -0500

    Fix 0 task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,26 +14,8 @@ def add_integer(a, b=98):
     Returns:
         int: a + b casted to integer
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-
-    if not isinstance(b, int) and not isinstance(b, float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    if isinstance(a, float):
-        a = int(a)
-
-    if isinstance(b, float):
-        b = int(b)
-
-    if a != a:
-        raise ValueError
-    if b != b:
-        raise ValueError
-
-    result = a + b
-
-    if result == float("inf") or result == -float("inf"):
-        raise OverflowError
-
-    return (result)
+    return (int(a) + int(b))
