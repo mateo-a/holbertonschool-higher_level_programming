27554e6e2398b185973f60e1c8390e76e5752891
mateo-a <alexander.urrego@gmail.com>
2020-01-21 22:00:13 -0500

    Fix file 6

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -15,4 +15,4 @@ def from_json_string(my_str):
     Returns:
         An object (Python data structure) represented by a JSON string
     """
-    return json.loads(my_obj)
+    return json.loads(my_str)
