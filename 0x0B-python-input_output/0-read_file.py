b37155c11712fdd98d7daed8b32419422f0ee952
mateo-a <alexander.urrego@gmail.com>
2020-01-21 20:19:35 -0500

    Fix file 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     Function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
