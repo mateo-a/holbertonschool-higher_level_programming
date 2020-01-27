406d037bda9d3aa3b60c601f318d4f2317b29424
mateo-a <alexander.urrego@gmail.com>
2020-01-27 17:42:43 -0500

    Fix3 test_square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -343,29 +343,29 @@ class test_square(unittest.TestCase):
             pass
 
     def test_json_string_type(self):
-            '''
-                Testing the returned type
-            '''
-            list_input = [
-                {'id': 2089, 'size': 10},
-                {'id': 2712, 'size': 1}]
-            json_list_input = Square.to_json_string(list_input)
-            list_output = Square.from_json_string(json_list_input)
-            self.assertEqual(type(list_input), list)
+        '''
+            Testing the returned type
+        '''
+        list_input = [
+            {'id': 2089, 'size': 10},
+            {'id': 2712, 'size': 1}]
+        json_list_input = Square.to_json_string(list_input)
+        list_output = Square.from_json_string(json_list_input)
+        self.assertEqual(type(list_input), list)
 
     def test_json_string(self):
-            '''
-                Testing that the json string gets converted into a list
-            '''
-            list_input = [
-                {'id': 2089, 'size': 10},
-                {'id': 2712, 'size': 7}]
-            json_list_input = Square.to_json_string(list_input)
-            list_output = Square.from_json_string(json_list_input)
-            s1 = {'id': 2089, 'size': 10}
-            s2 = {'size': 7, 'id': 2712}
-            self.assertEqual(list_input[0], s1)
-            self.assertEqual(list_input[1], s2)
+        '''
+            Testing that the json string gets converted into a list
+        '''
+        list_input = [
+            {'id': 2089, 'size': 10},
+            {'id': 2712, 'size': 7}]
+        json_list_input = Square.to_json_string(list_input)
+        list_output = Square.from_json_string(json_list_input)
+        s1 = {'id': 2089, 'size': 10}
+        s2 = {'size': 7, 'id': 2712}
+        self.assertEqual(list_input[0], s1)
+        self.assertEqual(list_input[1], s2)
 
     def test_dict_to_instance(self):
         '''
