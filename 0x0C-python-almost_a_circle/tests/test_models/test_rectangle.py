ec48b6c0d8518916a0f2819526716e13139668bb
mateo-a <alexander.urrego@gmail.com>
2020-01-27 20:34:25 -0500

    Fix7 tests3

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -7,10 +6,44 @@ Rectangle = Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    def test_getter_setter(self):
+    def setUp(self):
+        """ Set up for all methods """
         Base._Base__nb_objects = 0
-        r1 = Rectangle(5, 2)
+
+    def test_nowidth(self):
+        """ Test for no width """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle()
+
+    def test_noheight(self):
+        """ Test for no height """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1)
+
+    def test_nox(self):
+        """ Test for no x """
+        r1 = Rectangle(1, 1)
+        self.assertEqual(r1.x, 0)
+
+    def test_noy(self):
+        """ Test for no y """
+        r1 = Rectangle(1, 1, 1)
+        self.assertEqual(r1.y, 0)
+
+    def test_noid(self):
+        """ Test for no id """
+        r1 = Rectangle(1, 1, 1, 1)
         self.assertEqual(r1.id, 1)
+
+    def test_id(self):
+        """ Test for id """
+        r1 = Rectangle(1, 1, 1, 1, 85)
+        self.assertEqual(r1.id, 85)
+
+    def test_extraarg(self):
+        """ Test for no extra arguments """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 1, 1, 1, 1, 1)
 
 
 if __name__ == '__main__':
