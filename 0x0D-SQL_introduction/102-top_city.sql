9a402e3b8562a9fb69178a746e02755e614b4eb8
mateo-a <alexander.urrego@gmail.com>
2020-03-02 21:45:04 -0500

    [Add] Add Tasks

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- Script that displays the top 3 of cities temperature during July and August
+SELECT city, AVG(value) as avg_temp
+       FROM temperatures
+       WHERE month = 7 OR month = 8
+       GROUP BY city
+       ORDER BY AVG(value) DESC LIMIT 3;
