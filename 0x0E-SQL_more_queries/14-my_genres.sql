5e5ec882cf70052c20d5526389a39f874b9f3ffe
mateo-a <alexander.urrego@gmail.com>
2020-03-03 22:52:47 -0500

    [Fix] Tasks 13

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,5 @@
--- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show
+-- Dexter
 SELECT gr.name
        FROM tv_genres gr, tv_show_genres sh_gr, tv_shows sh
        WHERE sh.title = 'Dexter'
