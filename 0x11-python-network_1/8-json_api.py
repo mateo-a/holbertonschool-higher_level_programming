acf7d8c4fa608805394a1934775c362d69ce7610
mateo-a <alexander.urrego@gmail.com>
2020-04-15 17:06:49 -0500

    [ADD] Add Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        req = requests.post(url, data={"q": ""})
+    else:
+        req = requests.post(url, data={"q": sys.argv[1]})
+    try:
+        jason = req.json()
+    except ValueError as e:
+        print("Not a valid JSON")
+    if not jason:
+        print("No result")
+    else:
+        print("[{}] {}".format(jason.get("id"), jason.get("name")))
