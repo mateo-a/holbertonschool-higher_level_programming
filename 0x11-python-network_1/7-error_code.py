1f731a80b7f1febd031e9d62a3a1a9697801125f
mateo-a <alexander.urrego@gmail.com>
2020-04-15 16:06:24 -0500

    [ADD] Add Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == "__main__":
-    req = requests.get(sys.argv[1]
+    req = requests.get(sys.argv[1])
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
