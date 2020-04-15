71634d4371ed8d755cbbc8e0d7fb682833ad1bb1
mateo-a <alexander.urrego@gmail.com>
2020-04-15 13:55:13 -0500

    [ADD] Add Tasks

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response."""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = dict(response.info())
+        if "X-Request-Id" in html:
+            print(html["X-Request-Id"])
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8)"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values).encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+    print(body.decode("utf-8"))
