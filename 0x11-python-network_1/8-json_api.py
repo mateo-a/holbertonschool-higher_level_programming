9351db356a56b3964926e5979adc88e9807046d5
mateo-a <alexander.urrego@gmail.com>
2020-04-15 17:29:34 -0500

    [ADD] Add Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,15 +7,15 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) == 1:
+    if len(sys.argv) > 1:
         req = requests.post(url, data={"q": ""})
     else:
         req = requests.post(url, data={"q": sys.argv[1]})
     try:
         jason = req.json()
-    except ValueError as e:
+        if not jason:
+            print("No result")
+        else:
+            print("[{}] {}".format(jason.get("id"), jason.get("name")))
+    except ValueError:
         print("Not a valid JSON")
-    if not jason:
-        print("No result")
-    else:
-        print("[{}] {}".format(jason.get("id"), jason.get("name")))
