2d609b470e040ea2715b9b434ebe94e685e2f165
mateo-a <alexander.urrego@gmail.com>
2020-05-05 17:18:35 -0500

    [Add] Add task 16

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // function that increments and calls a function
 exports.addMeMaybe = function (number, theFunction) {
-    theFunction(number + 1);
-  };
+  theFunction(number + 1);
+};
