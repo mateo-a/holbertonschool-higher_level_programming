be88a3eca3c77da9b47ad3cc55c523cc2c3f7b47
mateo-a <alexander.urrego@gmail.com>
2020-05-05 15:52:16 -0500

    [Fix] Fix task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints x times “C is fun”
-let n = process.argv[2];
-if (!isNaN(parseInt(n))) {
+if (!isNaN(parseInt(process.argv[2]))) {
+  let n = process.argv[2];
   for (let i = 0; i < n; i++) {
     console.log('C is fun');
   }
