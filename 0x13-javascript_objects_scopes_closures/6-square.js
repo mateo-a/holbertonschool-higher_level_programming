9fe26df34a1cfcc43a9b46374e9be6e558ba085c
mateo-a <alexander.urrego@gmail.com>
2020-05-11 20:03:40 -0500

    [Add] Add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const SquareFive = require('./5-square');
+
+class Square extends SquareFive {
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width ; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
