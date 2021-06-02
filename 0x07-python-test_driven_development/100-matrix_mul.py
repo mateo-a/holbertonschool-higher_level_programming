790f17f68872213b58c249b2da9a4a82e83dba16
John Alexander Urrego Sandoval <55117782+mateo-a@users.noreply.github.com>
2021-06-02 15:03:35 -0500

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,10 +6,7 @@ function that multiplies 2 matrices
 
 def check_args(matrix, name):
     """Check values"""
-    checkm = [len(i) for i in matrix]
-    if len(set(checkm)) != 1:
-        raise TypeError("each row of {} must be of the same size".format(name))
-        
+           
     if not isinstance(matrix, list):
         raise TypeError("{} must be a list".format(name))
     rows_length = []
