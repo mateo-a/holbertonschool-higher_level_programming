8800d1cb4a711d093c5f9a8f5b99b99ab8d828a6
John Alexander Urrego Sandoval <55117782+mateo-a@users.noreply.github.com>
2021-06-02 15:02:10 -0500

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,8 +8,7 @@ def check_args(matrix, name):
     """Check values"""
     checkm = [len(i) for i in matrix]
     if len(set(checkm)) != 1:
-        msg = "each row of m_a must be of the same size"
-        raise TypeError(msg)
+        raise TypeError("each row of {} must be of the same size".format(name))
         
     if not isinstance(matrix, list):
         raise TypeError("{} must be a list".format(name))
