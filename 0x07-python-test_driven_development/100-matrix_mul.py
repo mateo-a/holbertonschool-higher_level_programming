13f0444a2333040392448bab54a6bb36b158eed4
John Alexander Urrego Sandoval <55117782+mateo-a@users.noreply.github.com>
2021-06-02 14:45:58 -0500

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -10,8 +10,7 @@ def check_args(matrix, name):
     """Check values"""
     if not isinstance(matrix, list):
         raise TypeError("{} must be a list".format(name))
-    '''if not all(isinstance(i, list) for i in matrix):
-        raise TypeError("{} must be a list of lists".format(name))'''
+    
     rows_length = []
     for row in matrix:
         if not isinstance(row, list):
